d4d7dc8fc30c13dc4a0c1b4cbcd15964d1da79e2
Fergus <fdmacpherson@gmail.com>
2024-01-18 13:51:19 +1100

    Initial

---
+++ common.gradle.kt
@@ -0,0 +1,29 @@
+apply(plugin = "maven-publish")
+apply(plugin = "java")
+
+project.repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+project.configure<org.gradle.api.publish.PublishingExtension> {
+    publications {
+        register("mavenJava", org.gradle.api.publish.maven.MavenPublication::class) {
+            from(components["java"])
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
+
+project.tasks.named("publishToMavenLocal").configure {
+    dependsOn("assemble")
+}
+
+project.tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+
+project.group = "fergusm"
